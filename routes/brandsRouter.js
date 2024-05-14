f94b9aba34e446e0d8b97d990230ee1cc3b19b4f
Mariangelica <gromarant@gmail.com>
2024-05-14 10:06:17 +0200

    add methods get, post, patch, put and delete into brands router

--- routes/brandsRouter.js
+++ routes/brandsRouter.js
@@ -2,15 +2,67 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('return = All products order by brand');
+router.get('/filter', (req, res) => {
+  res.status(200).send('this is a brand filter');
 });
 
-router.get('/:brandId', (req, res) => {
+router.get('/:brandId?', (req, res) => {
   const { brandId } = req.params;
-  res.json(
+
+  if( brandId ) {
+    res.status(200).json(
+      {
+        brand_id: brandId,
+      }
+    )
+  }
+  else {
+    res.status(200).send('return = All products order by brand');
+  }
+});
+
+router.post('/', (req,res) => {
+  const body = req.body;
+
+  res.status(201).json(
     {
-      brand_id: brandId,
+      message: 'created brand with id: ' + body.id,
+      data: body,
+    }
+  )
+});
+
+router.patch('/:brandId', (req,res) => {
+  const { brandId } = req.params;
+
+  res.status(200).json(
+    {
+      message: 'Brand with id:' + brandId + ' updated',
+      data: req.body,
+      id: brandId,
+    }
+  )
+});
+
+router.put('/:brandId', (req,res) => {
+  const { brandId } = req.params;
+
+  res.status(200).json(
+    {
+      message: 'Brand with id:' + brandId + ' updated',
+      data: req.body,
+      id: brandId,
+    }
+  )
+});
+
+router.delete('/:brandId', (req,res) => {
+  const { brandId } = req.params;
+
+  res.status(200).json(
+    {
+      message: "Product deleted",
+      id: brandId,
     }
   )
 });
