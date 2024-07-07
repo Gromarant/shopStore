a1131311d322fe68ce2bcd54b5b5e7d797d82377
Mariangelica <gromarant@gmail.com>
2024-07-07 20:09:05 +0200

    add cleaner button into cart input

--- client/src/atomComponents/LabelAmount.jsx
+++ client/src/atomComponents/LabelAmount.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoMdCloseCircle } from "react-icons/io";
 
 function LabelAmount({title, amount, currency,  unit, edit}) {
     let [count, setCount] = useState(parseFloat(amount).toFixed(2));
@@ -6,7 +7,10 @@ function LabelAmount({title, amount, currency,  unit, edit}) {
     return (
         <article>
             <p className='font_6'>{title}</p>
-            {edit ? <input className='input price_input' type='number' onChange={e => setCount(e.target.value)} value={count}/>
+            {edit ? <form className='input flex_r'>
+                        <input className='price_input' type='number' onChange={e => setCount(e.target.value)} value={count}/>
+                        <IoMdCloseCircle className='close' onClick={() => setCount(0)}/>
+                    </form>
                   :   <section className='flex_r label_amount'>
                         <h1 className='font_12'>{amount + currency}</h1>
                         <p className='font_6'>{unit}</p>
