a1bc6418ae6edda52fc41eb5721b9bc1c332d44e
Mariangelica <gromarant@gmail.com>
2024-07-07 18:23:56 +0200

    update Counter.jsx

--- client/src/atomComponents/Counter.jsx
+++ client/src/atomComponents/Counter.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import { BsTrash } from "react-icons/bs";
 
 function Counter({count}) {
+    let [counter, setCounter] = useState(count);
+
+    const handleCounter = (sign) => {
+        if (counter <= 0 && sign === '-') { 
+            setCounter(0);
+            //delete element from the list
+        } else if (sign === '-') { 
+            setCounter(counter--); 
+        } else { 
+            setCounter(counter++); 
+        }
+    }
+    
     return (
         <section className='flex_r counter'>
-            <button className='btn unvisible'>{'-'}</button>
-            <p>{count}</p> 
-            <button className='btn unvisible'>{'+'}</button> 
+            <button className='btn unvisible' onClick={() => handleCounter('-')}>{'-'}</button>
+            <input className='cart_input' type='number' onChange={e => setCounter(e.target.value)} value={counter}/>
+            <button className='btn unvisible' onClick={() => handleCounter('+')}>{'+'}</button> 
             <div className='trash_container'>
                 <BsTrash className='trash'/>
             </div>
