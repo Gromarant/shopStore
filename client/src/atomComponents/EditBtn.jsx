8e52822931ad2d429cc0251d78d9707f6a48c7ad
Mariangelica <gromarant@gmail.com>
2024-07-08 18:03:46 +0200

    add EditBtn.jsx

---
+++ client/src/atomComponents/EditBtn.jsx
@@ -0,0 +1,11 @@
+import { MdOutlineEdit } from "react-icons/md";
+
+function EditBtn() {
+    return (
+    <div className='edit'>
+        <MdOutlineEdit className='icon'/>
+    </div>
+    );
+};
+
+export default EditBtn;
