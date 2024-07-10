4f3eb52b9e09eae8564a252beddcbb0537a62f71
Mariangelica <gromarant@gmail.com>
2024-07-10 22:33:07 +0200

    update List adding ShoppingCard.jsx

---
+++ client/src/components/ShoppingCard.jsx
@@ -0,0 +1,28 @@
+import { Logo, Image, LabelAmount, Counter } from '../atomComponents';
+
+function ShoppingCard({id, logo, img, name, brand, labelOne, labelTwo, handle, count}) {
+    return (
+        <article className='card shopList'>
+            <header className='header'>
+                <Logo img={logo}/>
+                <div className='check flex_r'>
+                    <input className='checkbox' id={id} type='checkbox' onChange={(e) => handle(e.target)}/>
+                </div>
+            </header>
+            <section className='content'>
+                <Image img={img}/>
+                <section className='card_text'>
+                    <h1 className='font_12'>{name}</h1>
+                    <p className='font_8'>{brand}</p>
+                    <section className='flex_r price_contaner'>
+                        <LabelAmount data={{title: labelOne.title, amount: labelOne.amount, currency: labelOne.currency,  unit: labelOne.unit, edit: false}}/>
+                        <LabelAmount data={{title: labelTwo.title, amount: labelTwo.amount, currency: labelTwo.currency,  unit: labelTwo.unit, edit: false}}/>
+                    </section>
+                    {count ? <Counter count={count}/> : null}
+                </section>
+            </section>
+        </article>
+    );
+};
+
+export default ShoppingCard;
--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -1,74 +1,40 @@
-import Counter from '../atomComponents/Counter';
-import Header from '../atomComponents/Header';
-import Image from '../atomComponents/Image';
-import LabelAmount from '../atomComponents/LabelAmount';
-
-let content = {
-    // id: 
-    header: 'check',
-    logo: 'https://www.supeco.net/wp-content/uploads/2019/04/logo-carr%C3%A9.jpg',
-    url: 'https://images.unsplash.com/photo-1490818387583-1baba5e638af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGZvb2R8ZW58MHx8MHx8fDA%3D',
-    // nickname: 'tomatoe sauce',
-    name: 'keptchup',
-    brand: 'carrefour',
-    labelOne: {
-        title: 'to paid: ',
-        amount: '1.50',
-        currency: ' €',
-        unit: 'package'
-    },
-    labelTwo: {
-        title: 'Price per unit / kg / L: ',
-        amount: '1.30',
-        currency: '€',
-        unit: 'Kg'
-    },
-    // contentBar: 25
-    count: 2
-}
+import { useContext } from 'react';
+import { logoStore, pricePerKg } from '../utils/card';
+import { shoppingListContext } from '../context/shoppingListContext';
+import { ShoppingCard } from '../components';
 
 
-function Card({type, header, content}) {
-    const { logo, url, name, brand, labelOne, labelTwo, nickname, contentBar, count } = content;
-    const edit = () => type === 'shopListEdit' ? true : false;
-
-    return (
-        <article className={`cart ${type}`}>
-            <Header type={header} logo={logo}/>
-            <section className='content'>
-                <Image img={url}/>
-                <section className='cart_text'>
-                    {type === 'pantry' && contentBar ? <progress className='content_bar' value={contentBar} max={100} /> : null}
-                    { nickname ? 
-                        <>
-                        <h1 className='font_12'>{nickname}</h1>
-                        <p className='font_12'>{name}</p></>  
-                        : <h1 className='font_12'>{name}</h1>}
-                    <p className='font_8'>{brand}</p>
-                    <section className='flex_r price_contaner'>
-                        <LabelAmount data={{title: labelOne.title, amount: labelOne.amount, currency: labelOne.currency,  unit: labelOne.unit, edit: edit()}}/>
-                        <LabelAmount data={{title: labelTwo.title, amount: labelTwo.amount, currency: labelTwo.currency,  unit: labelTwo.unit, edit: edit()}}/>
-                    </section>
-                    {count ? <Counter count={count}/> : null}
-                </section>
-            </section>
-        </article>
-    );
-};
 
 
 function List() {
-    
+    const {shoppingList} = useContext(shoppingListContext);
+    const handleCheckedELement = (element) => console.log(element);
+
     return (
-        <>
-           <Card type={'shopListEdit'} header={'check'} content={content}/>
-           <Card type={'search'} header={'check'} content={content}/>
-           <Card type={'shopList'} header={'check'} content={content}/>
-           <Card type={'search'} header={'check'} content={content}/>
-           <Card type={'shopList'} header={'check'} content={content}/>
-           <Card type={'pantry'} header={'check'} content={content}/>
-           <Card type={'pantry'} header={'edit'} content={content}/>
-        </>
+        <section className='search'>
+            { shoppingList?.map(product => 
+            <ShoppingCard
+            id={product.uid}
+            handle={handleCheckedELement}
+            logo={logoStore(product.store)} 
+            img={product.img}
+            name={product.name}
+            brand={product.store}
+            labelOne={{
+                title: 'to paid: ',
+                amount: product.price,
+                currency: ' €',
+                unit: ` ${product.content} ${product.measure}`
+            }} 
+            labelTwo={{
+                title: 'Price per: ',
+                amount: pricePerKg(product.measure, parseFloat(product.price), product.content),
+                currency: '€',
+                unit: 'Kg'
+            }}
+            count={1}
+            key={product.uid}/>)}
+        </section>
     )
 };
 
