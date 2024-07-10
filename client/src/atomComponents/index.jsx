c96f558d20e34433953c35d71bf507865dce6938
Mariangelica <gromarant@gmail.com>
2024-07-10 21:44:20 +0200

    update exports and card util functions

---
+++ client/src/atomComponents/index.jsx
@@ -0,0 +1,25 @@
+import BillCard from "./BillCard";
+import CheckInput from "./CheckInput";
+import ClearInput from "./ClearInput";
+import Counter from "./Counter";
+import DropDown from "./DropDown";
+import EditBtn from "./EditBtn";
+import Header from "./Header";
+import Image from "./Image";
+import LabelAmount from "./LabelAmount";
+import Logo from "./Logo";
+import NavLink from "./NavLink";
+
+export {
+    BillCard,
+    CheckInput,
+    ClearInput,
+    Counter,
+    DropDown,
+    EditBtn,
+    Header,
+    Image,
+    LabelAmount,
+    Logo,
+    NavLink
+};
---
+++ client/src/components/index.jsx
@@ -0,0 +1,9 @@
+import { NavBar } from './NavBar';
+import { SearchBar } from './SearchBar';
+import { SearchCard } from './SearchCard';
+
+export {
+    NavBar,
+    SearchBar,
+    SearchCard
+};
