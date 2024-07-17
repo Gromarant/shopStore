100a08e0465e8b8ef5a77f9ae96f3ef14f07909e
Mariangelica <gromarant@gmail.com>
2024-07-17 21:05:04 +0200

    clean List page

--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -8,10 +8,9 @@ function List() {
     const [displayShoppingList, setDisplayShoppingList] = useState(false);
     const [catalog, setCatalog] = useState([]);
     const [shoppingList, setShoppingList] = useState({});
-    const [currentList, setCurrentList] = useState([]);
 
-    const setIntoList = (productId, quantity) => {
-        setShoppingList(oldList => ({ ...oldList, [productId]: Number(quantity) }));
+    const setIntoList = (product, quantity) => {
+        setShoppingList(oldList => ({ ...oldList, [product.uid]: { ...product, quantity: Number(quantity) } }));
     };
 
     const setProducts = async() => {
@@ -21,10 +20,7 @@ function List() {
 
     const setList = async() => { 
         const list = await shoppingListModel.getListItems();
-        if (list) {
-            list.map( item => setIntoList(item.uid, item.quantity))
-        }
-        setCurrentList(list);
+        list.map( item => setIntoList(item, item.quantity));
     };
 
     useEffect(() => {
@@ -32,27 +28,29 @@ function List() {
         setProducts();
     }, []);
 
-    // console.log('shoppingList', shoppingList);
-    // console.log('currentList', currentList);
+    const getCleanShoppingList = (objectList) => Object.values(objectList)?.filter(product => product.quantity !== 0);
+
     const createListItems = () => {
-        const cleanShoppingList = Object.entries(shoppingList)
-        .filter(([, quantity]) => quantity !== 0)
-        .map(([uid, quantity]) => ({uid, quantity}));
+        const cleanShoppingList = getCleanShoppingList(shoppingList)
         shoppingListModel.postList(cleanShoppingList);
-        setList();
     };
+
+    const shoppingListElements = ['shopList', getCleanShoppingList(shoppingList), FaRegEyeSlash]
+    const catalogListElements = ['search', catalog, FaRegEye]
+
+    const [style, data, ToggleIcon] = displayShoppingList ? shoppingListElements : catalogListElements;
 
     return(
         <section className='search'>
-            { displayShoppingList && currentList?.map(product => 
+            { data.map(product => 
             <RegularCard
-                style={'shopList'}
+                style={style}
                 id={product.product_uid}
                 logo={logoStore(product.store)} 
                 img={product.img}
                 name={product.name}
                 brand={product.store}
-                setQuantity={quantity => setIntoList(product.product_uid, quantity)}
+                setQuantity={quantity => setIntoList(product, quantity)}
                 labelOne={{
                     title: 'to paid: ',
                     amount: product.price,
@@ -65,34 +63,12 @@ function List() {
                     currency: '€',
                     unit: 'Kg'
                 }}
-                count={shoppingList[product.uid]}
-                key={product.uid}/>)}
-
-            { !displayShoppingList && catalog?.map(product => 
-            <RegularCard
-                style={'search'}
-                id={product.uid}
-                logo={logoStore(product.store)} 
-                img={product.img}
-                name={product.name}
-                brand={product.store}
-                setQuantity={quantity => setIntoList(product.uid, quantity)}
-                labelOne={{
-                    title: 'to paid: ',
-                    amount: product.price,
-                    currency: ' €',
-                    unit: ` ${product.content} ${product.measure}`
-                }} 
-                labelTwo={{
-                    title: 'Price per: ',
-                    amount: pricePerKg(product.measure, parseFloat(product.price), product.content),
-                    currency: '€',
-                    unit: 'Kg'
-                }}
-                count={shoppingList[product.uid]}
+                count={shoppingList[product.uid]?.quantity}
                 key={product.uid}/>)}
             <button className='cta createList' onClick={() => createListItems()}>Create List</button>
-            <div className='icon_div menu' onClick={() => setDisplayShoppingList(!displayShoppingList)}>{displayShoppingList ? <FaRegEyeSlash className="icon"/> : <FaRegEye className="icon"/>}</div>
+            <div className='icon_div menu' onClick={() => setDisplayShoppingList(!displayShoppingList)}>
+                <ToggleIcon className="icon"/>
+            </div>
         </section>
     )
 };
