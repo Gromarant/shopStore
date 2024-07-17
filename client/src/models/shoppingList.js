5fae61dfd19d0ae4ee8cdd9bf32e903c6ffa21be
Mariangelica <gromarant@gmail.com>
2024-07-17 20:01:59 +0200

    add getListItems methods into list models

--- client/src/models/shoppingList.js
+++ client/src/models/shoppingList.js
@@ -4,6 +4,12 @@ const postList = async(list) => {
     await axios.post(`/api/v1/${import.meta.env.VITE_END_POINT_LIST}`, list);
 };
 
+const getListItems = async() => {
+    const { data: response } = await axios.get(`/api/v1/${import.meta.env.VITE_END_POINT_LIST}`);
+    return response;
+};
+
 export default {
-    postList
+    postList,
+    getListItems
 };
